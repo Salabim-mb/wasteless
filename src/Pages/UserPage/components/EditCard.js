e9236c3854dcc044430dd7b1ad6be23717578221
kczaj <kubapcz@gmail.com>
2021-02-02 15:13:20 +0100

    Add integration with backend for changing password

--- src/Pages/UserPage/components/EditCard.js
+++ src/Pages/UserPage/components/EditCard.js
@@ -2,7 +2,7 @@ import {Avatar, Button, Container, Icon, Paper, TextField} from "@material-ui/co
 import React, {useContext, useRef, useState} from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import EditIcon from "@material-ui/icons/Edit";
-import {AlertContext} from "../../../context";
+import {AlertContext, UserContext} from "../../../context";
 
 const useStyles = makeStyles((theme) => ({
     mainDiv: {
@@ -57,19 +57,56 @@ const useStyles = makeStyles((theme) => ({
         width: "80%"
     },
 }))
+const checkPasswordFormat = (body) => {
+    const oldPass = body.old_password
+    const newPass = body.new_password1
+    const newPassR = body.new_password2
 
-export default function EditCard({user}) {
+    if (oldPass.length < 8 || newPassR.length < 8 || newPass.length < 8 || !/^[a-zA-Z0-9!@#$%&*]+$/.test(oldPass) || !/^[a-zA-Z0-9!@#$%&*]+$/.test(newPass) || !/^[a-zA-Z0-9!@#$%&*]+$/.test(newPassR)) {
+        throw "Wrong password format."
+    }
+
+    if (newPass !== newPassR) {
+        throw "New password and repeat password must be the same."
+    }
+
+}
+
+const changePassword = async (body, token) => {
+    const url = "https://wasteless-backend.herokuapp.com/profile/changepassword";
+    const headers = {
+        "Content-Type": "application/json",
+        Authorization: "Token " + token
+    };
+
+    const res = await fetch(url, {
+        headers,
+        method: "PUT",
+        body: JSON.stringify(body),
+    });
+
+    if (res.status !== 200) {
+        throw "Couldn't change your password. Make sure you put right old password."
+    }
+}
+
+export default function EditCard() {
     const classes = useStyles();
     const [oldPass, setOldPass] = useState("")
     const [newPass, setNewPass] = useState("")
     const [newPassR, setNewPassR] = useState("")
     const alertC = useRef(useContext(AlertContext));
+    const user = useContext((UserContext))
 
     const handleSubmitPass = async (e) => {
+        e.preventDefault()
         try {
-            console.log("click")
+            let body = {old_password: oldPass, new_password1: newPass, new_password2: newPassR}
+            checkPasswordFormat(body)
+            await changePassword(body, user.token)
+            alertC.current.showAlert("Successfully changed password.", "success")
         } catch (err) {
-            alertC.current.showAlert("Couldn't change your password", "error")
+            alertC.current.showAlert(err, "error")
         }
     }
 
@@ -80,7 +117,9 @@ export default function EditCard({user}) {
                     <div className={classes.profileDiv}>
                         <div className={classes.avatarDiv}>
                             <div>
-                                <Avatar className={classes.avatar}>{user.name.charAt(0).toUpperCase()}</Avatar>
+                                {user.avatarImg === "" ?
+                                    <Avatar className={classes.avatar}>{user?.data?.name.charAt(0).toUpperCase()}</Avatar> :
+                                    <Avatar className={classes.avatar} src={user?.data?.avatarImg}/>}
                             </div>
                             <div className={classes.editBtn}>
                                 <Button size="small" variant="contained" endIcon={<EditIcon/>}>
@@ -90,24 +129,28 @@ export default function EditCard({user}) {
 
                         </div>
                         <div className={classes.detailsDiv}>
-                            <TextField className={classes.textField} label="Name" defaultValue={user.name}
+                            <TextField className={classes.textField} label="Name" defaultValue={user?.data?.name}
                             />
-                            <TextField className={classes.textField} label="Surname" defaultValue={user.surname}
+                            <TextField className={classes.textField} label="Surname" defaultValue={user?.data?.surname}
                             />
-                            <TextField className={classes.textField} label="Email" defaultValue={user.email}
+                            <TextField className={classes.textField} label="Email" defaultValue={user?.data?.email}
                             />
                             <TextField className={classes.textField} label="Username"
-                                       defaultValue={user.username}
+                                       defaultValue={user?.data?.username}
                             />
                             <div className={classes.submitBtn}>
                                 <Button variant="contained" color="primary" endIcon={<Icon>send</Icon>}>Submit</Button>
                             </div>
                             <h2>CHANGE PASSWORD</h2>
-                            <TextField className={classes.textField} label="Recent password" type="password" onChange={(e) => setOldPass(e.target.value)}/>
-                            <TextField className={classes.textField} label="New password" type="password" onChange={(e) => setNewPass(e.target.value)}/>
-                            <TextField className={classes.textField} label="Repeat password" type="password" onChange={(e) => setNewPassR(e.target.value)}/>
+                            <TextField className={classes.textField} label="Recent password" type="password"
+                                       onChange={(e) => setOldPass(e.target.value)}/>
+                            <TextField className={classes.textField} label="New password" type="password"
+                                       onChange={(e) => setNewPass(e.target.value)}/>
+                            <TextField className={classes.textField} label="Repeat password" type="password"
+                                       onChange={(e) => setNewPassR(e.target.value)}/>
                             <div className={classes.submitBtn}>
-                                <Button variant="contained" color="primary" endIcon={<Icon>send</Icon>} onClick={handleSubmitPass}>Submit</Button>
+                                <Button variant="contained" color="primary" endIcon={<Icon>send</Icon>}
+                                        onClick={handleSubmitPass}>Submit</Button>
                             </div>
                         </div>
                     </div>
