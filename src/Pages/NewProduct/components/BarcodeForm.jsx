c04a3fc574534a8c9b0e78682c44475d61327046
Krisu17 <01142550@pw.edu.pl>
2021-02-03 11:44:18 +0100

    Working barcode reader, but only with presented images

--- src/Pages/NewProduct/components/BarcodeForm.jsx
+++ src/Pages/NewProduct/components/BarcodeForm.jsx
@@ -1,7 +1,66 @@
 import React from 'react';
 import {Grid, TextField} from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { BrowserBarcodeReader } from '@zxing/library';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+    input: {
+        display: 'none',
+    },
+}));
+
+function onFileSelected(event) {
+    var selectedFile = event.target.files[0];
+    if(selectedFile !== undefined) {
+        var reader = new FileReader();
+
+        var imgtag = document.getElementById("barcodeImg");
+        imgtag.title = selectedFile.name;
+
+        reader.onload = function(event) {
+            imgtag.src = event.target.result;
+        };
+
+        reader.readAsDataURL(selectedFile);
+    }
+
+}
+
+const decodeFromImage = async (e) =>{
+
+
+    let codeReader = new BrowserBarcodeReader();
+    let img = document.getElementById("barcodeImg");
+    let result = "";
+    let barcodeFieldId = document.getElementById("barcode");
+    // codeReader.decodeFromImage(img)
+    //     .then(result => {
+    //         console.log(result);
+    //
+    //         barcodeFieldId.value = result.text;
+    //     })
+    //     .catch(err => {
+    //         console.error(err);
+    //         barcodeFieldId.value = err;
+    //     });
+
+    try {
+        result = await codeReader.decodeFromImage(img);
+    } catch (err) {
+        console.error(err);
+    }
+    barcodeFieldId.value = result.text;
+    console.log(result);
+}
 
 const BarcodeForm = ({data, setData}) => {
+    const classes = useStyles();
     return (
         <>
             <Grid item xs={12}>
@@ -18,6 +77,28 @@ const BarcodeForm = ({data, setData}) => {
                     onChange={e => setData(e.target.value)}
                 />
             </Grid>
+            <img id="barcodeImg" height="200" />
+            <div className={classes.root}>
+                <input
+                    accept="image/*"
+                    className={classes.input}
+                    id="contained-button-file"
+                    multiple
+                    type="file"
+                    onChange= {e => onFileSelected(e)}
+                />
+                <label htmlFor="contained-button-file">
+                    <Button variant="contained" color="primary" component="span">
+                        Upload
+                    </Button>
+                </label>
+                <input accept="image/*" className={classes.input} id="icon-button-file" type="file" />
+                <label>
+                    <Button variant="contained" color="default" component="span" onClick={ e => decodeFromImage(e)}>
+                        Decode
+                    </Button>
+                </label>
+            </div>
         </>
     )
 };
