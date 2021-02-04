04527f6217bcdd24dcfbd4f6ff064950a467d7ab
pawellski <skibaapawell@gmail.com>
2021-02-04 15:17:09 +0100

    First version of Settings Card

--- src/Pages/UserPage/components/SettingsCard.js
+++ src/Pages/UserPage/components/SettingsCard.js
@@ -1,12 +1,23 @@
 import {Container, Paper} from "@material-ui/core";
 import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
+import Switch from '@material-ui/core/Switch';
+import {Typography} from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => {
+const useStyles = makeStyles((theme) => ({
     mainDiv: {
+        margin: theme.spacing(3),
+        marginBottom: theme.spacing(8),
+    },
+    settingsDiv: {
+        display: "block",
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    elementDiv: {
 
-    }
-})
+    },
+}))
 
 export default function SettingsCard({user}) {
     const classes = useStyles();
@@ -14,10 +25,33 @@ export default function SettingsCard({user}) {
         <Container fixed>
             <Paper>
                 <div className={classes.mainDiv}>
-
+                    <div className={classes.settingsDiv}>
+                        <div className={classes.elementDiv}>
+                            <Typography>Dark mode</Typography>
+                            <Switch
+                                color="primary"
+                                name="checkedB"
+                                inputProps={{ 'aria-label': 'primary checkbox' }}
+                            />
+                        </div>
+                        <div className={classes.elementDiv}>
+                            <Typography>Email Notifications</Typography>
+                            <Switch
+                                color="primary"
+                                name="checkedB"
+                                inputProps={{ 'aria-label': 'primary checkbox' }}
+                            />
+                        </div>
+                        <div className={classes.elementDiv}>
+                            <Typography>Push Notifications</Typography>
+                            <Switch
+                                color="primary"
+                                name="checkedB"
+                                inputProps={{ 'aria-label': 'primary checkbox' }}
+                            />
+                        </div>
+                    </div>
                 </div>
-                CONSENT
-                {user.name}
             </Paper>
         </Container>
     )
