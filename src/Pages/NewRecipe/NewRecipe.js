d604dc39358884b9fdb813e60f39b8c4a84b2354
kczaj <kubapcz@gmail.com>
2021-04-09 22:11:52 +0200

    Adding ability to add own ingredient

--- src/Pages/NewRecipe/NewRecipe.js
+++ src/Pages/NewRecipe/NewRecipe.js
@@ -13,7 +13,7 @@ import {
     ListItemAvatar,
     ListItemSecondaryAction,
     ListItemText,
-    MenuItem,
+    MenuItem, Modal,
     Paper,
     Select,
     TextField,
@@ -31,7 +31,7 @@ import {path_list} from "../../constants/routes";
 import {Redirect} from "react-router-dom";
 import {fileToBase64} from "../../utils/fileToBase64";
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import Autocomplete, {createFilterOptions} from '@material-ui/lab/Autocomplete';
 
 const useStyles = makeStyles((theme) => ({
     mainDiv: {
@@ -69,6 +69,9 @@ const useStyles = makeStyles((theme) => ({
         listStyle: 'none',
         padding: theme.spacing(0.5),
         margin: 0,
+    },
+    buttons: {
+        margin: theme.spacing(1)
     }
 }))
 
@@ -97,10 +100,27 @@ async function fetchIngredientsList(token) {
     });
 
     if (res.status !== 200) {
-        throw "Couldn't create recipe."
+        throw "Couldn't get ingredients list"
     }
 
     return await res.json()
+}
+
+async function fetchNewIngredient(token, ingredient_name) {
+    const url = be.INGREDIENTS;
+    const headers = getCORSHeaders(token);
+
+    const res = await fetch(url, {
+        headers,
+        method: "POST",
+        body: JSON.stringify({
+            ingredient_name: ingredient_name
+        })
+    });
+
+    if (res.status !== 201) {
+        throw "Couldn't add new ingredient"
+    }
 }
 
 export default function NewRecipe() {
@@ -124,16 +144,27 @@ export default function NewRecipe() {
     const [tag, setTag] = useState("");
     const [tags, setTags] = React.useState([]);
     const [ingredientsList, setIngredientsList] = useState([]);
+    const [open, setOpenModal] = React.useState(false);
+    const filter = createFilterOptions();
+
+    const handleClose = () => {
+        setOpenModal(false);
+    };
+
+    const handleOpen = () => {
+        setOpenModal(true);
+    };
 
     useEffect(() => {
-        loadIngredientList()
+        loadIngredientList();
     }, [])
 
     const loadIngredientList = async () => {
         try {
             let res = await fetchIngredientsList(user.token);
+            res.forEach((e) => e.title = e.ingredient_name)
             setIngredientsList(res)
-        } catch(err) {
+        } catch (err) {
             alertC.current.showAlert(err, "error")
         }
 
@@ -350,6 +381,12 @@ export default function NewRecipe() {
         }
     }
 
+    const body = (
+        <div className={classes.modalPaper}>
+
+        </div>
+    );
+
     return (
         <React.Fragment>
             <CssBaseline/>
@@ -432,15 +469,47 @@ export default function NewRecipe() {
                                 </Select>
                             </FormControl>
                             <Autocomplete
-                                id="combo-box-demo"
+                                freeSolo
+                                onChange={async (event, newValue) => {
+                                    if (newValue && newValue.id) {
+                                        // timeout to avoid instant validation of the dialog's form.
+                                        setIngredient(newValue.ingredient_name)
+                                    } else if (newValue) {
+                                        try{
+                                            if (!/^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ\s]+$/.test(newValue.ingredient_name)) {
+                                                throw "Wrong ingredient format"
+                                            }
+                                            await fetchNewIngredient(user.token, newValue.ingredient_name)
+                                            setIngredient(newValue.ingredient_name)
+                                            loadIngredientList();
+                                        } catch (err){
+                                            alertC.current.showAlert(err, "error")
+                                        }
+                                    } else {
+                                        setIngredient("")
+                                    }
+                                }}
+                                filterOptions={(options, params) => {
+                                    const filtered = filter(options, params);
+
+                                    if (params.inputValue !== '') {
+                                        let element = {
+                                            title: "Add " + params.inputValue,
+                                            ingredient_name: params.inputValue,
+                                        }
+                                        filtered.push(element);
+                                    }
+
+                                    return filtered;
+                                }}
                                 options={ingredientsList}
-                                getOptionLabel={(option) => option.ingredient_name}
-                                style={{ width: 300 }}
-                                renderInput={(params) => <TextField {...params} label="Ingredient" variant="outlined" />}
+                                getOptionLabel={(option) => option.title}
+                                className={classes.textField}
+                                renderInput={(params) => <TextField {...params} label="Ingredient" variant="outlined"/>}
                             />
                         </div>
                         <div className={classes.floatingButton}>
-                            <Fab size="small" color="secondary" aria-label="add" className={classes.margin}
+                            <Fab size="small" color="secondary" aria-label="add" className={classes.buttons}
                                  onClick={handleAddIngredient}>
                                 <AddIcon/>
                             </Fab>
@@ -505,6 +574,14 @@ export default function NewRecipe() {
                     </div>
                     {redirect && <Redirect to={redirect}/>}
                 </Paper>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="simple-modal-title"
+                    aria-describedby="simple-modal-description"
+                >
+                    {body}
+                </Modal>
 
             </Container>
 
